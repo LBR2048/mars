a94c70abd8bc641f2704315509a7214f09ef3d0c
LBR2048 <LBR2048@users.noreply.github.com>
2016-12-27 08:38:37 -0600

    Delete Main.java

--- src/mars/Main.java
+++
@@ -1,9 +0,0 @@
-package mars;
-
-public class Main {
-	public static void main(String[] args) {
-		Mars mars = new Mars();
-		String result = mars.move("MMRMMRMMg");
-		System.out.println(result);
-	}
-}
